1b2864ae82cddf28903cd6fc814b110fe04ee6bb
Andrii Krasnychuk <andrii@deity.io>
2021-05-19 19:44:09 +0200

    GA test 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactGA from 'react-ga';
 
 const TRACKING_ID = 'UA-197518751-1'; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rootElement = document.getElementById('root');
 
