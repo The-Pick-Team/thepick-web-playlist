9b22729cdf43ce02307eaad107accdf50c1a35eb
Andrii Krasnychuk <andrii@deity.io>
2021-05-19 17:43:13 +0200

    added google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from 'app/App/App';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-V7JMEKRZNL'; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const rootElement = document.getElementById('root');
 
